9a31d8c82ce9119ccd6c18f923b8950df4165cf5
vaibhav18matere <vpm00718897@gmail.com>
2024-05-22 20:15:38 +0530

    ui layout defined and pages added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import LeftSidenav from "./pages/Sidenavigation";
+import Feed from "@/components/Feed";
+import RightSideNav from "./pages/RightSideNav";
+
 export default function Home() {
-  return <main>Twitter Clone</main>;
+  return (
+    <>
+      <div className="grid grid-cols-12 h-screen w-screen text-white py-6">
+        <LeftSidenav />
+        <Feed />
+        <RightSideNav />
+      </div>
+    </>
+  );
 }
---
+++ app/pages/RightSideNav.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RightSideNav = () => {
+  return (
+    <div className="col-span-3 pl-4">
+      Whats happening, Who to follow section
+    </div>
+  );
+};
+
+export default RightSideNav;
